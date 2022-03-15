5e386d71b5e84aba5ac23dbc11a0d6971373dbe8
Cloud-Sure <101168687+Cloud-Sure@users.noreply.github.com>
2022-03-15 15:30:53 +0800

    Add files via upload

---
+++ Practical5/pizza_party.py
@@ -0,0 +1,3 @@
+p= (n**2+n+2)/2
+p=64 #p= input()
+print (n)
---
+++ Practical5/variables.py
@@ -0,0 +1,11 @@
+a=19245301 #2022
+b=4218520 #2021
+c=271 #2020
+d=b-c
+e=a-b
+if d>e: print ("d is grater")
+if d<e: print ("e is grater")
+f=e/b #rate	of	new	cases in 2021
+g=d/c #rate	of	new	cases in 2020
+if f>g: print ("rate of new cases in 2021 is grater")
+if f<g: print ("rate of new cases in 2020 is grater")
