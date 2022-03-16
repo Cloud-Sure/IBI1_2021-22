2803fc44845689684fb53e2fc5d8906af7ee23e8
Zhang Yunshuo <101168687+Cloud-Sure@users.noreply.github.com>
2022-03-16 14:39:37 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,9 +3,9 @@ b=4218520 #2021
 c=271 #2020
 d=b-c
 e=a-b
-if d>e: print ("d is grater")
-if d<e: print ("e is grater")
+if d>e: print ("d is greater")
+if d<e: print ("e is greater")
 f=e/b #rate	of	new	cases in 2021
 g=d/c #rate	of	new	cases in 2020
-if f>g: print ("rate of new cases in 2021 is grater")
-if f<g: print ("rate of new cases in 2020 is grater")
+if f>g: print ("rate of new cases in 2021 is greater")
+if f<g: print ("rate of new cases in 2020 is greater")
