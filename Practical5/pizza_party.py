22eaa31da266d20ace96b471d9579237a0c53282
Zhang Yunshuo <101168687+Cloud-Sure@users.noreply.github.com>
2022-03-23 16:39:03 +0800

    Update pizza_party.py
    A correct version was uploaded

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,3 +1,8 @@
-p= (n**2+n+2)/2
-p=64 #p= input()
-print (n)
+n=1 #Set an initial value
+while ((n**2+n+2)/2)<999: #Set a loop, but not the number here
+    if ((n**2+n+2)/2)<64:
+        print('Cut'+str(n)+'straight lines, we well get'+str((n**2+n+2)/2)+'pieces of pizza. but we steel need more!' )
+        n+=1
+    else:
+        print('Cut'+str(n)+'straight lines, we well get'+str((n**2+n+2)/2)+'pieces of pizza. it is enough for us!' )
+        break #Terminate the loop here
