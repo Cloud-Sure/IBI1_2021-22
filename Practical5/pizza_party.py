59db82c89be445e6abc7ba65f018a3c7fe421253
Zhang Yunshuo <101168687+Cloud-Sure@users.noreply.github.com>
2022-05-25 00:52:28 +0800

    Update pizza_party.py
    The comment for n is refined in pizza_party.py in week5.

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,4 +1,4 @@
-n=1 #Set an initial value
+n=1 #Set an initial value（n is the initial number of cuts, which will then be added up as the while cycles）
 while ((n**2+n+2)/2)<999: #Set a loop, but not the number here
     if ((n**2+n+2)/2)<64:
         print('Cut'+str(n)+'straight lines, we well get'+str((n**2+n+2)/2)+'pieces of pizza. but we steel need more!' )
